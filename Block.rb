6a3273e2e78f55f683117a814f3e2cb22a6a40a2
Davidson <davidsonts@outlook.com>
2020-09-13 17:49:33 -0300

    add mine

--- Block.rb
+++ Block.rb
@@ -2,15 +2,27 @@ require 'digest'
 
 class Block
   attr_reader :index, :previousHash, :data, :timestamp, :hash
-  def initialize(index = 0, previousHash = nil, data = 'Genesis block')
+  def initialize(index = 0, previousHash = nil, data = 'Genesis block', difficulty = 4)
     @index = index
     @previousHash = previousHash
     @data = data
     @timestamp = Time.now
-    @hash = self.generateHash
+    @difficulty = difficulty
+    @nonce = 0
+    self.mine
   end
 
   def generateHash
-    return Digest::SHA2.new(256).hexdigest [@index, @previousHash, @data, @timestamp].to_s
+    return Digest::SHA2.new(256).hexdigest [@index, @previousHash, @data, @timestamp, @nonce].to_s
   end
+
+  def mine
+    @hash = self.generateHash
+    str_zero = '0' * @difficulty
+    until @hash[0..@difficulty - 1].match?str_zero,0 do
+      @nonce +=1
+      @hash = self.generateHash
+    end
+  end
+  
 end
